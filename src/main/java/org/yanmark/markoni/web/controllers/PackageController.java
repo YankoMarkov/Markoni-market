7e4c6b53d381e8e8631d4d7ba6525506cf982b2b
Yanko <yanko@markov.eu>
2019-04-04 23:18:45 +0300

    add create package

--- src/main/java/org/yanmark/markoni/web/controllers/PackageController.java
+++ src/main/java/org/yanmark/markoni/web/controllers/PackageController.java
@@ -1,6 +1,12 @@
 package org.yanmark.markoni.web.controllers;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import org.yanmark.markoni.domain.entities.Status;
 import org.yanmark.markoni.domain.models.bindings.packages.PackageCreateBindingModel;
 import org.yanmark.markoni.domain.models.services.PackageServiceModel;
@@ -10,12 +16,6 @@ import org.yanmark.markoni.domain.models.views.packages.PackageDetailsViewModel;
 import org.yanmark.markoni.domain.models.views.users.UserViewModel;
 import org.yanmark.markoni.services.PackageService;
 import org.yanmark.markoni.services.UserService;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
 import java.time.LocalDateTime;
@@ -26,72 +26,73 @@ import java.util.stream.Collectors;
 @Controller
 @RequestMapping("/packages")
 public class PackageController extends BaseController {
-	
-	private final PackageService packageService;
-	private final UserService userService;
-	private final ModelMapper modelMapper;
-	
-	@Autowired
-	public PackageController(PackageService packageService, UserService userService, ModelMapper modelMapper) {
-		this.packageService = packageService;
-		this.userService = userService;
-		this.modelMapper = modelMapper;
-	}
-	
-	@GetMapping("/create")
-	public ModelAndView create(@ModelAttribute("packageCreate") PackageCreateBindingModel packageCreate,
-	                           ModelAndView modelAndView) {
-		List<UserViewModel> userViewModels = this.userService.getAllUsers().stream()
-				.map(user -> this.modelMapper.map(user, UserViewModel.class))
-				.collect(Collectors.toList());
-		modelAndView.addObject("users", userViewModels);
-		return this.view("packageCreate", modelAndView);
-	}
-	
-	@PostMapping("/create")
-	public ModelAndView createConfirm(@Valid @ModelAttribute("packageCreate") PackageCreateBindingModel packageCreate,
-	                                  BindingResult bindingResult) {
-		if (bindingResult.hasErrors()) {
-			return this.view("packageCreate");
-		}
-		UserServiceModel userServiceModel = this.userService.getUserById(packageCreate.getRecipient());
-		if (userServiceModel.getProducts().isEmpty()) {
-			return this.view("packageCreate");
-		}
-		PackageServiceModel packageServiceModel = this.modelMapper.map(packageCreate, PackageServiceModel.class);
-		packageServiceModel.setRecipient(userServiceModel);
-		packageServiceModel.setShippingAddress(userServiceModel.getAddress());
-		packageServiceModel.setStatus(Status.PENDING);
-		packageServiceModel.setEstimatedDeliveryDay(LocalDateTime.now());
-		double weight = userServiceModel.getProducts().stream()
-				.mapToDouble(ProductServiceModel::getWeight).sum();
-		packageServiceModel.setWeight(weight);
-		StringBuilder description = new StringBuilder();
-		userServiceModel.getProducts().forEach(product -> description.append(product.getName()).append(System.lineSeparator()));
-		packageServiceModel.setDescription(description.toString());
-		if (this.packageService.savePackage(packageServiceModel) == null) {
-			return this.view("packageCreate");
-		}
-		return this.redirect("/home");
-	}
-	
-	@GetMapping("/details")
-	public ModelAndView details(@RequestParam("id") String id, ModelAndView modelAndView) {
-		PackageServiceModel packageServiceModel = this.packageService.getPackageById(id);
-		PackageDetailsViewModel packageDetailsViewModel = this.modelMapper.map(packageServiceModel, PackageDetailsViewModel.class);
-		packageDetailsViewModel.setRecipient(packageServiceModel.getRecipient().getUsername());
-		if (packageServiceModel.getStatus().equals(Status.PENDING)) {
-			packageDetailsViewModel.setEstimatedDeliveryDay("N/A");
-		} else {
-			if (packageServiceModel.getStatus().equals(Status.DELIVERED) || packageServiceModel.getStatus().equals(Status.ACQUIRED)) {
-				packageDetailsViewModel.setEstimatedDeliveryDay("Delivered");
-			} else {
-				DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMM-yyyy");
-				String date = packageServiceModel.getEstimatedDeliveryDay().format(formatter);
-				packageDetailsViewModel.setEstimatedDeliveryDay(date);
-			}
-		}
-		modelAndView.addObject("package", packageDetailsViewModel);
-		return this.view("packageDetails", modelAndView);
-	}
+
+    private final PackageService packageService;
+    private final UserService userService;
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public PackageController(PackageService packageService, UserService userService, ModelMapper modelMapper) {
+        this.packageService = packageService;
+        this.userService = userService;
+        this.modelMapper = modelMapper;
+    }
+
+    @GetMapping("/create")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ModelAndView create(@ModelAttribute("packageCreate") PackageCreateBindingModel packageCreate,
+                               ModelAndView modelAndView) {
+        List<UserViewModel> userViewModels = this.userService.getAllUsers().stream()
+                .map(user -> this.modelMapper.map(user, UserViewModel.class))
+                .collect(Collectors.toList());
+        modelAndView.addObject("users", userViewModels);
+        return this.view("/packages/package-create", modelAndView);
+    }
+
+    @PostMapping("/create")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ModelAndView createConfirm(@Valid @ModelAttribute("packageCreate") PackageCreateBindingModel packageCreate,
+                                      BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return this.view("/packages/package-create");
+        }
+        UserServiceModel userServiceModel = this.userService.getUserById(packageCreate.getRecipient());
+        if (userServiceModel.getProducts().isEmpty()) {
+            return this.view("/packages/package-create");
+        }
+        PackageServiceModel packageServiceModel = this.modelMapper.map(packageCreate, PackageServiceModel.class);
+        packageServiceModel.setRecipient(userServiceModel);
+        packageServiceModel.setShippingAddress(userServiceModel.getAddress());
+        packageServiceModel.setStatus(Status.PENDING);
+        packageServiceModel.setEstimatedDeliveryDay(LocalDateTime.now());
+        double weight = userServiceModel.getProducts().stream()
+                .mapToDouble(ProductServiceModel::getWeight).sum();
+        packageServiceModel.setWeight(weight);
+        StringBuilder description = new StringBuilder();
+        userServiceModel.getProducts().forEach(product -> description.append(product.getName()).append(System.lineSeparator()));
+        packageServiceModel.setDescription(description.toString());
+        this.packageService.savePackage(packageServiceModel);
+        return this.redirect("/packages/pending");
+    }
+
+    @GetMapping("/details")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ModelAndView details(@RequestParam("id") String id, ModelAndView modelAndView) {
+        PackageServiceModel packageServiceModel = this.packageService.getPackageById(id);
+        PackageDetailsViewModel packageDetailsViewModel = this.modelMapper.map(packageServiceModel, PackageDetailsViewModel.class);
+        packageDetailsViewModel.setRecipient(packageServiceModel.getRecipient().getUsername());
+        if (packageServiceModel.getStatus().equals(Status.PENDING)) {
+            packageDetailsViewModel.setEstimatedDeliveryDay("N/A");
+        } else {
+            if (packageServiceModel.getStatus().equals(Status.DELIVERED) || packageServiceModel.getStatus().equals(Status.ACQUIRED)) {
+                packageDetailsViewModel.setEstimatedDeliveryDay("Delivered");
+            } else {
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMM-yyyy");
+                String date = packageServiceModel.getEstimatedDeliveryDay().format(formatter);
+                packageDetailsViewModel.setEstimatedDeliveryDay(date);
+            }
+        }
+        modelAndView.addObject("package", packageDetailsViewModel);
+        return this.view("packageDetails", modelAndView);
+    }
 }
--- src/main/java/org/yanmark/markoni/web/controllers/PendingController.java
+++ src/main/java/org/yanmark/markoni/web/controllers/PendingController.java
@@ -1,17 +1,18 @@
 package org.yanmark.markoni.web.controllers;
 
-import org.yanmark.markoni.domain.entities.Status;
-import org.yanmark.markoni.domain.models.services.PackageServiceModel;
-import org.yanmark.markoni.domain.models.views.packages.PendingAndDeliveredViewModel;
-import org.yanmark.markoni.services.PackageService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.yanmark.markoni.domain.entities.Status;
+import org.yanmark.markoni.domain.models.services.PackageServiceModel;
+import org.yanmark.markoni.domain.models.views.packages.PendingAndDeliveredViewModel;
+import org.yanmark.markoni.services.PackageService;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -21,40 +22,42 @@ import java.util.stream.Collectors;
 @Controller
 @RequestMapping("/packages")
 public class PendingController extends BaseController {
-	
-	private final PackageService packageService;
-	private final ModelMapper modelMapper;
-	
-	@Autowired
-	public PendingController(PackageService packageService, ModelMapper modelMapper) {
-		this.packageService = packageService;
-		this.modelMapper = modelMapper;
-	}
-	
-	@GetMapping("/pending")
-	public ModelAndView pendingPackages(ModelAndView modelAndView) {
-		List<PendingAndDeliveredViewModel> pendingAndDeliveredViewModels = this.packageService.getAllPackagesByStatus(Status.PENDING).stream()
-				.map(pack -> {
-					PendingAndDeliveredViewModel pendingAndDeliveredViewModel = this.modelMapper.map(pack, PendingAndDeliveredViewModel.class);
-					pendingAndDeliveredViewModel.setRecipient(pack.getRecipient().getUsername());
-					return pendingAndDeliveredViewModel;
-				})
-				.collect(Collectors.toList());
-		modelAndView.addObject("pendings", pendingAndDeliveredViewModels);
-		return this.view("pending", modelAndView);
-	}
-	
-	@PostMapping("/pending")
-	public ModelAndView shipPackage(@RequestParam String pendingId) {
-		PackageServiceModel packageServiceModel = this.packageService.getPackageById(pendingId);
-		packageServiceModel.setStatus(Status.SHIPPED);
-		packageServiceModel.setEstimatedDeliveryDay(LocalDateTime.now().plusDays(getRandomDays()));
-		this.packageService.savePackage(packageServiceModel);
-		return this.redirect("/packages/pending");
-	}
-	
-	private int getRandomDays() {
-		Random random = new Random();
-		return random.nextInt(20 - 5 + 1) + 5;
-	}
+
+    private final PackageService packageService;
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public PendingController(PackageService packageService, ModelMapper modelMapper) {
+        this.packageService = packageService;
+        this.modelMapper = modelMapper;
+    }
+
+    @GetMapping("/pending")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ModelAndView pendingPackages(ModelAndView modelAndView) {
+        List<PendingAndDeliveredViewModel> pendingAndDeliveredViewModels = this.packageService.getAllPackagesByStatus(Status.PENDING).stream()
+                .map(pack -> {
+                    PendingAndDeliveredViewModel pendingAndDeliveredViewModel = this.modelMapper.map(pack, PendingAndDeliveredViewModel.class);
+                    pendingAndDeliveredViewModel.setRecipient(pack.getRecipient().getUsername());
+                    return pendingAndDeliveredViewModel;
+                })
+                .collect(Collectors.toList());
+        modelAndView.addObject("pendings", pendingAndDeliveredViewModels);
+        return this.view("/packages/pending", modelAndView);
+    }
+
+    @PostMapping("/pending")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ModelAndView shipPackage(@RequestParam String pendingId) {
+        PackageServiceModel packageServiceModel = this.packageService.getPackageById(pendingId);
+        packageServiceModel.setStatus(Status.SHIPPED);
+        packageServiceModel.setEstimatedDeliveryDay(LocalDateTime.now().plusDays(getRandomDays()));
+        this.packageService.savePackage(packageServiceModel);
+        return this.redirect("/packages/pending");
+    }
+
+    private int getRandomDays() {
+        Random random = new Random();
+        return random.nextInt(20 - 5 + 1) + 5;
+    }
 }
